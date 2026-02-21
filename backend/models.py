6545d2785034a185d852055bd6615e073ef10502
Palak887 <24f1002229@ds.study.iitm.ac.in>
2026-02-21 19:44:04 +0530

    Session 4

--- backend/models.py
+++ backend/models.py
@@ -6,26 +6,26 @@ class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
-    chapters = db.relationship("Chapter", backref="course", cascade="all,delete", lazy=True)
+    questions = db.relationship("Question", backref="course", cascade="all,delete", lazy=True)
 
 
-class Chapter(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    no_of_question = db.Column(db.Integer, default = 0)
-    description = db.Column(db.String)
-    course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=False)
-    quizzes = db.relationship("Quiz", backref="chapter", cascade="all,delete", lazy=True)
+# class Chapter(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     no_of_question = db.Column(db.Integer, default = 0)
+#     description = db.Column(db.String)
+#     course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=False)
+#     quizzes = db.relationship("Quiz", backref="chapter", cascade="all,delete", lazy=True)
                             
 
-class Quiz(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    date_time = db.Column(db.DateTime, nullable=False)
-    remarks = db.Column(db.String)
-    chapter_id = db.Column(db.Integer, db.ForeignKey("chapter.id"), nullable=False)
-    scores = db.relationship("Scores", backref="quiz", cascade="all,delete", lazy=True)
-    questions = db.relationship("Question", backref='quiz', cascade='all,delete', lazy=True)
+# class Quiz(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     date_time = db.Column(db.DateTime, nullable=False)
+#     remarks = db.Column(db.String)
+#     chapter_id = db.Column(db.Integer, db.ForeignKey("chapter.id"), nullable=False)
+#     scores = db.relationship("Scores", backref="quiz", cascade="all,delete", lazy=True)
+#     questions = db.relationship("Question", backref='quiz', cascade='all,delete', lazy=True)
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -36,14 +36,13 @@ class Question(db.Model):
     option3 = db.Column(db.String, nullable=False)
     option4 = db.Column(db.String, nullable=False)
     correct_option = db.Column(db.String, nullable=False)
-    quiz_id = db.Column(db.Integer, db.ForeignKey("quiz.id"), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=False)
 
 
 class Scores(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     time_stamp_of_attempt = db.Column(db.Time)
     total_scored = db.Column(db.Integer, default = 00)
-    quiz_id = db.Column(db.Integer, db.ForeignKey("quiz.id"), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
 
--- backend/routes.py
+++ backend/routes.py
@@ -13,9 +13,9 @@ def login():
         password = request.form.get("password")
         user = User.query.filter_by(email = email , password = password).first()
         if user and user.role == 0:
-            return redirect(url_for("admin", email = email))
+            return redirect(url_for("admin", email=email))
         elif user and user.role == 1:
-            return redirect(url_for("user", email= email))
+            return redirect(url_for("user", email=email))
         else:
             return render_template("Login.html", msg = "Invalid user")
         
@@ -38,52 +38,109 @@ def Register():
 
 @app.route("/admin/<email>")
 def admin(email):
-    courses = get_courses()
+    courses = get_course()
     return render_template("admin.html", email=email, courses=courses)
 
 
-def get_courses(id=None):
+def get_course(id=None):
     if id:
         return Course.query.filter_by(id=id).first()
     return Course.query.all()
 
 
-@app.route("/user/<email>")
-def user(email):
-    courses = get_courses()
-    return render_template("User.html", email=email, courses=courses)
-
-
-@app.route("/add_course/<email>", methods=["GET", "POST"])
+@app.route("/add_course/<email>", methods=["GET","POST"])
 def add_course(email):
-    if request.method=="POST":
-        id = request.form.get("id")
-        name = request.form.get("name")
-        description = request.form.get("description")
-        newcourse = Course (id = id, name = name, description=description)
-        db.session.add(newcourse)
-        db.session.commit()
-        return redirect(url_for("admin", email=email))
-    return render_template("add_course.html", email=email)
-    
-
-@app.route("/edit_course/<id>/<email>", methods=['GET', 'POST'])
-def edit_course(id, email):
-    c = get_courses(id)
     if request.method == "POST":
         id = request.form.get("id")
         name = request.form.get("name")
         description = request.form.get("description")
-        c.id = id
-        c.name = name
-        c.description = description
+        newcourse = Course(id = id, name=name, description=description)
+        db.session.add(newcourse)
         db.session.commit()
         return redirect(url_for("admin", email=email))
+    return render_template("add_course.html", email=email)
+
+
+@app.route("/edit_course/<id>/<email>", methods = {"GET","POST"})
+def edit_course(id, email):
+    c = get_course(id)
+    if request.method == "POST":
+        id = request.form.get("id")
+        name = request.form.get("name")
+        description = request.form.get("description")
+        c.id = id 
+        c.name = name 
+        c.description = description
+        db.session.commit()
+        return redirect(url_for("admin", email = email))
     return render_template("edit_course.html", email=email, course = c)
 
 @app.route("/delete_course/<id>/<email>", methods=["GET","POST"])
-def delete_course(id,email):
-    dc = get_courses(id)
+def delete_course(id, email):
+    dc = get_course(id)
     db.session.delete(dc)
+    db.session.commit()
+    return redirect(url_for("admin", email=email))
+
+
+@app.route("/user/<email>")
+def user(email):
+    courses = get_course()
+    return render_template("user.html", email=email, courses=courses)
+
+
+@app.route("/add_question/<course_id>/<email>", methods=["GET", "POST"])
+def add_question(course_id, email):
+    if request.method == "POST":
+        id =request.form.get("ques_id")
+        title = request.form.get("title")
+        question_statement = request.form.get("question_statement")
+        option1 = request.form.get("option1")
+        option2 = request.form.get("option2")
+        option3 = request.form.get("option3")
+        option4 = request.form.get("option4")
+        correct_option_key = request.form.get("correct_option")
+
+        correct_option_mapping = {"option1" : option1 , "option2" : option2, "option3" : option3 , "option4" : option4}
+        correct_option = correct_option_mapping.get(correct_option_key, "")
+
+        new_question = Question(id = id , title=title, question_statement=question_statement, option1=option1, option2=option2, option3=option3, option4=option4, correct_option=correct_option, course_id=course_id)
+        db.session.add(new_question)
+        db.session.commit()
+        return redirect(url_for("admin", email=email))
+    return render_template("add_question.html", course_id=course_id, email=email)
+
+@app.route("/edit_question/<id>/<email>", methods=["GET","POST"])
+def edit_question(id, email):
+    que = get_question(id)
+    if request.method == "POST":
+        title = request.form.get("title")
+        question_statement = request.form.get("question_statement")
+        option1 = request.form.get("option1")
+        option2 = request.form.get("option2")
+        option3 = request.form.get("option3")
+        option4 = request.form.get("option4")
+        correct_option = request.form.get("correct_option")
+        que.title = title
+        que.question_statement = question_statement
+        que.option1 = option1
+        que.option2 = option2
+        que.option3 = option3
+        que.option4 = option4
+        que.correct_option = correct_option
+        db.session.commit()
+        return redirect(url_for("admin", email = email))
+    return render_template("edit_question.html", email = email, question = que)
+    
+
+def get_question(id):
+    questions = Question.query.filter_by(id=id).first()
+    return questions
+
+
+@app.route("/delete_question/<id>/<email>", methods = ["GET", "POST"])
+def delete_question(id, email):
+    dque = get_question(id)
+    db.session.delete(dque)
     db.session.commit()
     return redirect(url_for("admin", email=email))
